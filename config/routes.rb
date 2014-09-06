f9e190e1864ed38398a5c021970ca28eec25d7ac
jamesfwz <james@futureworkz.com>
2014-09-06 20:31:53 +0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Simple::Application.routes.draw do
 
   resources :products, except: :destroy
 
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'products#index'
 
